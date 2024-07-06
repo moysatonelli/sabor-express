f8a9c2a4fc1af7ffb366afd7c6033210436e4dd3
Moyses Tonelli <moysato.br@gmail.com>
2024-07-06 09:23:14 -0300

    versão 2

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -27,18 +27,21 @@ def opcao_invalida():
     main()
 
 def escolher_opcao():
-    opcao_escolhida = int(input("Escolha uma opção: "))
-    print(f"Você escolheu a opção {opcao_escolhida}")
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+        print(f"Você escolheu a opção {opcao_escolhida}")
 
-    if opcao_escolhida == 1:
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3:
-        print("Ativar restaurante")
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
+        if opcao_escolhida == 1:
+            print("Cadastrar restaurante")
+        elif opcao_escolhida == 2:
+            print("Listar restaurantes")
+        elif opcao_escolhida == 3:
+            print("Ativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
         opcao_invalida()
 
 def main():
