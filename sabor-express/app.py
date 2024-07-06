04f234a8d335b11508b75d55a2ce3899fb1215e9
Moyses Tonelli <moysato.br@gmail.com>
2024-07-06 12:25:55 -0300

    versão 5

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -20,32 +20,35 @@ def exibir_opcoes():
     print("4. Sair\n")
 
 def finalizar_app():
-    os.system('cls')
-    print("Finalizando o app\n")
+    exibir_subtitulo("Finalizar app")
 
-def opcao_invalida():
-    print("Opção inválida\n")
-    input("Digite uma tecla para voltar ao menu principal: ")
-    main()
-
-def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
-    nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
-    restaurantes.append(nome_do_restaurante)
-    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+def voltar_ao_menu_principal():
     input("\nDigite uma tecla para voltar ao menu principal: ")
     main()
 
-def listar_restaurantes():
+def opcao_invalida():
+    print("Opção inválida\n")
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
     os.system('cls')
-    print('Listando os restaurantes\n')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo("Cadastro de novos restaurantes")
+    nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
+    restaurantes.append(nome_do_restaurante)
+    print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo("Listando restaurantes")
 
     for restaurante in restaurantes:
         print(f".{restaurante}")
 
-    input("\nDigite uma tecla para voltar ao menu principal: ")
-    main()
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
