a1513e3ef2519eec842afc718c7fdbaf75f4f6ac
Moyses Tonelli <moysato.br@gmail.com>
2024-07-06 12:08:18 -0300

    versão 4

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ["Pizza","Sushi"]
 
 def exibir_nome_do_programa():
     print("""
@@ -34,7 +34,17 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input("Digite o nome do restaurante que deseja cadastrar: ")
     restaurantes.append(nome_do_restaurante)
     print(f"O restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
-    input("Digite uma tecla para voltar ao menu principal: ")
+    input("\nDigite uma tecla para voltar ao menu principal: ")
+    main()
+
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f".{restaurante}")
+
+    input("\nDigite uma tecla para voltar ao menu principal: ")
     main()
 
 def escolher_opcao():
@@ -45,7 +55,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print("Listar restaurantes")
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print("Ativar restaurante")
         elif opcao_escolhida == 4:
